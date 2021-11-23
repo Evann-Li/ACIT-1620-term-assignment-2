b8d92c1b158fa54c9ace2e509ea71a5587d8cc9d
unknown <evannli450@gmail.com>
2021-11-22 20:54:07 -0800

    render view

--- script.js
+++ script.js
@@ -55,3 +55,45 @@ function cleanUpView() {
         nodes[i].remove()
     }
 }
+
+function renderView(contacts){
+    let parent = document.querySelector(".main")
+    let child = document.createElement("div");
+    child.className = "contactinfo";
+    let childName = document.createElement("div");
+    childName.className = "contactname";
+    childName.textContent = contacts.name;
+    let pic = document.createElement("img");
+    pic.className = "profilepic";
+    pic.src="./img/profile.jpg";
+    pic.alt = "Profile Picture";
+    let childEmail = document.createElement("div");
+    childEmail.className = "contactemail";
+    childEmail.textContent = "email: " + contacts.email;
+    let childPhone = document.createElement("div");
+    childPhone.className = "contactphone";
+    childPhone.textContent = "phone: " + contacts.phone;
+    let childAddress = document.createElement("div");
+    childAddress.className = "contactaddress";
+    childAddress.textContent = "address: " + contacts.address;
+    let buttons = document.createElement("div")
+    buttons.className = "buttons"
+    let childEditButton = document.createElement("button");
+    childEditButton.className = "button edit";
+    childEditButton.textContent = "Edit";
+    let childCloseButton = document.createElement("button");
+    childCloseButton.className = "button close";
+    childCloseButton.textContent = "Close";
+
+    //append all children into their respective spots and ultimately into the child
+    childName.appendChild(pic)
+    child.appendChild(childName)
+    child.appendChild(childEmail)
+    child.appendChild(childPhone)
+    child.appendChild(childAddress)
+    buttons.append(childEditButton)
+    buttons.appendChild(childCloseButton)
+    child.appendChild(buttons)
+    //append the final version of the child to the parent
+    parent.appendChild(child);
+}
